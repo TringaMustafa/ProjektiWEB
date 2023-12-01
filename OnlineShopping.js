ef77315939715e73f2a37c190c0e1a0dd71edd21
TringaMustafa <tm64774@ubt-uni.net>
2023-12-01 10:23:09 +0100

    WEB

---
+++ OnlineShopping.js
@@ -0,0 +1,26 @@
+function validateForm(){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+
+    var nameRegex = /^[a-zA-Z\s]+$/;
+    if(!nameRegex.test(name)){
+        alert('Please enter a valid name');
+        return false;
+
+    }
+
+    var emailRegex =/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailRegex.test(email)){
+        alert('Please enter a valid email');
+        return false;
+
+    }
+
+    if(password.length < 6){
+        alert('Password must be at least 6 characters long');
+        return false;
+    }
+
+}
